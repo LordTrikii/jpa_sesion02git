74023911a97e3425854225eb95c02626774540d0
fernando <i202117412@cibertec-edu.pe>
2023-09-11 20:52:02 -0500

    Primer commit Fernnado Moran

---
+++ src/main/java/main/Pruebas.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import model.TbUsuario;
+
+public class Pruebas {
+
+	public static void main(String[] args) {
+	
+
+		System.out.println("===JPA CRUD====");
+		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("jpa_sesion02GIT");
+		EntityManager em = fabrica.createEntityManager();
+		
+		List<TbUsuario> lstUsuarios = em.createQuery("Select a From TbUsuario a", TbUsuario.class).getResultList();
+		System.out.println("==============================================");
+		
+		/*Mostrar cantidad de usuarios*/
+		System.out.println("nro de usuarios:" +lstUsuarios.size());
+		System.out.println("==============================================");
+		
+		
+		
+	}//fin dle metodo
+	
+
+}//fin de la clase
